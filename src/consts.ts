724791abe9c985d59d90e6549197b91a7a13ecd5
nishkaush <kaushish.nishant@hotmail.com>
2026-01-02 01:45:10 +1100

    migrated the whole site to astro

---
+++ src/consts.ts
@@ -0,0 +1,770 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Friends NGO";
+export const SITE_DESCRIPTION =
+  "Empowering people, protecting environment, providing health care services, implementing governmental schemes with the help of volunteers to induce positive changes at grass roots levels of the society.";
+
+export const projects = [
+  {
+    id: 1,
+    title: "Measles Rubella",
+    description:
+      "Every year in India nearly 2.8 million children get measles or rubella. The Measles Rubella (MR) program is an initiative by the Government of India which aims at vaccinating children aged 9 months to 15 years against these diseases. We have implemented this program in Roorkee area.",
+    thumbnail: "women-rally.jpg",
+    folderName: "rubella",
+    images: [
+      {
+        name: "women-rally.jpg",
+        alt: "Women rallying",
+      },
+      {
+        name: "women-blue-rally.jpg",
+        alt: "Women rally in groups",
+      },
+      {
+        name: "school-kids.jpg",
+        alt: "kids at school after vaccination",
+      },
+      {
+        name: "geeta-kids.jpg",
+        alt: "Looking after children",
+      },
+      {
+        name: "mum-with-kid.jpg",
+        alt: "a mother watches as her child gets vaccinated",
+      },
+      {
+        name: "geeta-with-kid.jpg",
+        alt: "Helping kids with vaccination",
+      },
+      {
+        name: "women-rally-center.jpg",
+        alt: "Women rallying for vaccination",
+      },
+      {
+        name: "kids-outside-school.jpg",
+        alt: "vaccinated school kids",
+      },
+      {
+        name: "docs-with-kids.jpg",
+        alt: "Our dependable doctors",
+      },
+      {
+        name: "kids-with-teachers.jpg",
+        alt: "teachers helping with kids vaccination",
+      },
+      {
+        name: "girls-in-pink.jpg",
+        alt: "little girls after vaccination",
+      },
+      {
+        name: "cute-girl.jpg",
+        alt: "young girl getting vaccinated",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Urban Primary Health Centers (UPHC's)",
+    description:
+      "Launched under the National Urban Health Mission (NUHM), this project provides healthcare and medical services to the urban slum population. We have been running it since August 2010 in the Roorkee Area.",
+    thumbnail: "asha-aware-2.jpg",
+    folderName: "uphc",
+    images: [
+      {
+        name: "asha-aware-2.jpg",
+        alt: "Asha spreading awareness about health",
+      },
+      {
+        name: "asha-aware.jpg",
+        alt: "Asha helping locals",
+      },
+      {
+        name: "baby-weigh.jpg",
+        alt: "Weighing a new born",
+      },
+      {
+        name: "bp-check-2.jpg",
+        alt: "Helping a patient",
+      },
+      {
+        name: "kid-inject.jpg",
+        alt: "Kid Vaccination",
+      },
+      {
+        name: "kid-vaccine.jpg",
+        alt: "Child Vaccination",
+      },
+      {
+        name: "care-bed.jpg",
+        alt: "Resting bed for patients",
+      },
+      {
+        name: "docs-sit.jpg",
+        alt: "Our Helpful Team",
+      },
+      {
+        name: "bp-happy.jpg",
+        alt: "Happy Patients",
+      },
+      {
+        name: "bp-check-3.jpg",
+        alt: "Blood Pressure check up",
+      },
+      {
+        name: "incubators.jpg",
+        alt: "Incubators for samples",
+      },
+      {
+        name: "shivir.jpg",
+        alt: "One of our Health Camps",
+      },
+      {
+        name: "lab-technician.jpg",
+        alt: "Our Lab Staff",
+      },
+      {
+        name: "sign-4.jpg",
+        alt: "Ganeshpur Center",
+      },
+      {
+        name: "shivir-2.jpg",
+        alt: "Health Camps",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Community Action on Harm Reduction Project (CAHR)",
+    description:
+      "Known as 'Hridaya Project', it was aimed at reducing transmission of HIV/AIDS among Injecting Drug Users (IDU's) as well as raising their concerns with the local bodies. It was implemented under FCRA funding from 2012 - 2016.",
+    thumbnail: "staff-kids.jpg",
+    folderName: "hridya",
+    images: [
+      {
+        name: "sign.jpg",
+        alt: "World AIDS Day signage",
+      },
+      {
+        name: "staff-kids.jpg",
+        alt: "Our staff helping kids in slums",
+      },
+      {
+        name: "h2.jpg",
+        alt: "Treat addicts nicely",
+      },
+      {
+        name: "h3.jpg",
+        alt: "people in favour of addicts",
+      },
+      {
+        name: "h7.jpg",
+        alt: "Drug addicts protesting",
+      },
+      {
+        name: "h8.jpg",
+        alt: "expecting respect",
+      },
+
+      {
+        name: "h4.jpg",
+        alt: "spreading awareness",
+      },
+      {
+        name: "h9.jpg",
+        alt: "Police",
+      },
+      {
+        name: "h10.jpg",
+        alt: "Police",
+      },
+      {
+        name: "h12.jpg",
+        alt: "Police",
+      },
+      {
+        name: "h6.jpg",
+        alt: "AIDS",
+      },
+      {
+        name: "h5.jpg",
+        alt: "talking about AIDS",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Targeted Intervention Project",
+    description:
+      "Assigned to us by Uttarkhand State AIDS Control Society in Dehradun, this project aimed at reducing the transmission of AIDS in slum areas around Haridwar. Through street plays and by providing medical help, we raised awareness on AIDS and ways to prevent it.",
+    thumbnail: "1.jpg",
+    folderName: "ti",
+    images: [
+      {
+        name: "1.jpg",
+        alt: "Police colleagues",
+      },
+      {
+        name: "3.jpg",
+        alt: "police academy",
+      },
+
+      {
+        name: "12.jpg",
+        alt: "police",
+      },
+      {
+        name: "13.jpg",
+        alt: "Office",
+      },
+
+      {
+        name: "16.jpg",
+        alt: "spreading awareness",
+      },
+      {
+        name: "18.jpg",
+        alt: "Police",
+      },
+      {
+        name: "Ti-1.jpg",
+        alt: "Police",
+      },
+      {
+        name: "Ti-5.jpg",
+        alt: "talking about AIDS",
+      },
+      {
+        name: "Ti-6.jpg",
+        alt: "Local play",
+      },
+      {
+        name: "Ti-7.jpg",
+        alt: "spreading awareness",
+      },
+      {
+        name: "Ti-8.jpg",
+        alt: "locals enjoying the play",
+      },
+      {
+        name: "Ti-9.jpg",
+        alt: "AIDS play",
+      },
+      {
+        name: "Ti-12.jpg",
+        alt: "Our staff",
+      },
+      {
+        name: "Ti-16.jpg",
+        alt: "AIDS program",
+      },
+      {
+        name: "Ti-19.jpg",
+        alt: "Showing safe sex methods",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Telecounselling Project",
+    description:
+      "Also know as 'Hridaya Project' is aimed at reducing transmission of HIV/AIDS among Injecting Drug Users (IDU's) through awareness campaings and medical support. It is implemented by the India HIV/AIDS Alliance. This project was implemented under FCRA category from 2012 - 2016.",
+    thumbnail: "https://dummyimage.com/600x400/eeeeee/888888.png&text=📷",
+    folderName: "",
+    images: [],
+  },
+];
+
+export const events = [
+  {
+    id: 1,
+    title: "Swachh Bharat Abhiyan (Clean India Mission)",
+    description:
+      "In accordance with Government of India's campaign for cleaning India, we demonstrated our participation by organising various cleaning campaigns in and around Roorkee.",
+    thumbnail: "3-closeup.jpg",
+    folderName: "clean-india",
+    images: [
+      {
+        name: "3-closeup.jpg",
+        alt: "Colleagues Cleaning indoors",
+      },
+      {
+        name: "3-out.jpg",
+        alt: "3 colleagues outdoors",
+      },
+      {
+        name: "4-people.jpg",
+        alt: "Team of 4 cleaning",
+      },
+      {
+        name: "bright.jpg",
+        alt: "Colleagues cleaning",
+      },
+      {
+        name: "fun.jpg",
+        alt: "Having fun",
+      },
+      {
+        name: "girl-power.jpg",
+        alt: "Girls cleaning",
+      },
+      {
+        name: "group-bricks.jpg",
+        alt: "Cleaning crew",
+      },
+      {
+        name: "3-in.jpg",
+        alt: "Three staff members cleaning",
+      },
+      {
+        name: "5-out.jpg",
+        alt: "Team of 5 cleaning",
+      },
+      {
+        name: "rahul-2.jpg",
+        alt: "Helping Locals",
+      },
+      {
+        name: "many-in.jpg",
+        alt: "Group cleaning",
+      },
+      {
+        name: "preeti.jpg",
+        alt: "Supervising others cleaning",
+      },
+      {
+        name: "rahul.jpg",
+        alt: "Having fun while cleaning",
+      },
+      {
+        name: "random.jpg",
+        alt: "People Cleaning",
+      },
+      {
+        name: "single-girl.jpg",
+        alt: "Single girl Cleaning",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Intensified Mission IndraDhanush",
+    description:
+      "This program aims to give 90% immunization coverage in India by 2020 so as to significantly reduce child mortality rate. We provide all necessary vaccines such as BCG (for tuberclosis), Pentavalent, Measles and Rubella.",
+    thumbnail: "advising.jpg",
+    folderName: "indradhanush",
+    images: [
+      {
+        name: "advising.jpg",
+        alt: "Advising patients",
+      },
+      {
+        name: "aware.jpg",
+        alt: "Spreading awareness",
+      },
+      {
+        name: "certificate.jpg",
+        alt: "Achievement",
+      },
+      {
+        name: "cry.jpg",
+        alt: "kid crying",
+      },
+      {
+        name: "happy-doc.jpg",
+        alt: "Happy doctor",
+      },
+      {
+        name: "hussle.jpg",
+        alt: "busy center",
+      },
+      {
+        name: "line.jpg",
+        alt: "Line of patients",
+      },
+      {
+        name: "muslim-2.jpg",
+        alt: "Child given dose",
+      },
+      {
+        name: "old-doc.jpg",
+        alt: "Doctor helping",
+      },
+      {
+        name: "patients.jpg",
+        alt: "Waiting Patients",
+      },
+      {
+        name: "ranjana.jpg",
+        alt: "Helping kids",
+      },
+      {
+        name: "room.jpg",
+        alt: "Medical Room",
+      },
+      {
+        name: "sign-2.jpg",
+        alt: "Sign for center",
+      },
+      {
+        name: "team.jpg",
+        alt: "Team Helping",
+      },
+      {
+        name: "tika.jpg",
+        alt: "Immunizing",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Mahila Aarogya Samiti (Women Health Society)",
+    description:
+      "A society consisting of 8-12 local women per community, who raise awareness on issues relating to health, nutrition, water and sanitation for women living in the slums. They then share their findings and feedback with us, so we can act swiftly in resolving their issues.",
+    thumbnail: "2.jpg",
+    folderName: "mas",
+    images: [
+      {
+        name: "1.jpg",
+        alt: "Our Sign",
+      },
+      {
+        name: "2.jpg",
+        alt: "Our staff with kids",
+      },
+      {
+        name: "3.jpg",
+        alt: "Kids Study",
+      },
+      {
+        name: "6.jpg",
+        alt: "Giving out food",
+      },
+      {
+        name: "7.jpg",
+        alt: "More food spread",
+      },
+      {
+        name: "8.jpg",
+        alt: "Typical house in slums",
+      },
+      {
+        name: "9.jpg",
+        alt: "Parents with kids",
+      },
+      {
+        name: "10.jpg",
+        alt: "kids huddled up",
+      },
+      {
+        name: "11.jpg",
+        alt: "kids with books",
+      },
+      {
+        name: "12.jpg",
+        alt: "kids in a group",
+      },
+      {
+        name: "13.jpg",
+        alt: "more kids in a group",
+      },
+      {
+        name: "14.jpg",
+        alt: "Poor kids",
+      },
+      {
+        name: "15.jpg",
+        alt: "Kids sitting and drawing",
+      },
+      {
+        name: "16.jpg",
+        alt: "Kids in Slums",
+      },
+      {
+        name: "17.jpg",
+        alt: "Slum lady volunteer",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "World AIDS Day",
+    description:
+      "As part of our Hridaya Project, we organised this event on World AIDS Day to distribute books, food and necessary supplies to the children in the slums near the main Haridwar city. This was carried out on 1 December 2013.",
+    thumbnail: "posing.jpg",
+    folderName: "aids-day",
+    images: [
+      {
+        name: "sign.jpg",
+        alt: "Our Sign",
+      },
+      {
+        name: "staff-kids.jpg",
+        alt: "Our staff with kids",
+      },
+      {
+        name: "study.jpg",
+        alt: "Kids Study",
+      },
+      {
+        name: "award-lady.jpg",
+        alt: "Lady Awarded",
+      },
+      {
+        name: "book-kid.jpg",
+        alt: "Kids with books",
+      },
+      {
+        name: "food-2.jpg",
+        alt: "Giving out food",
+      },
+      {
+        name: "food.jpg",
+        alt: "More food spread",
+      },
+      {
+        name: "kid-dad.jpg",
+        alt: "Parents with kids",
+      },
+      {
+        name: "kids-books.jpg",
+        alt: "kids with books",
+      },
+      {
+        name: "kids-more.jpg",
+        alt: "more kids in a group",
+      },
+      {
+        name: "kids-poor.jpg",
+        alt: "Poor kids",
+      },
+      {
+        name: "kids-sit.jpg",
+        alt: "Kids sitting and drawing",
+      },
+      {
+        name: "kids.jpg",
+        alt: "Kids in Slums",
+      },
+      {
+        name: "parents-kids.jpg",
+        alt: "Parents and their kids",
+      },
+      {
+        name: "partap.jpg",
+        alt: "Team member Helping",
+      },
+      {
+        name: "partap-2.jpg",
+        alt: "Giving out notebooks",
+      },
+      {
+        name: "path.jpg",
+        alt: "path to the slum",
+      },
+      {
+        name: "posing.jpg",
+        alt: "awarding a child",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Understanding and Addressing Adolescent Needs (UDAAN)",
+    description:
+      "A part of the larger program called Adolescent Reproductive and Sexual Health (ARSH), UDAAN focusses on educating the adolscent population about nutrition, reproductive health and hygiene, among other issues.Friends contributed to the training under this program in Roorkee.",
+    thumbnail: "training-1.jpg",
+    folderName: "udaan",
+    images: [
+      {
+        name: "training-1.jpg",
+        alt: "sign",
+      },
+      {
+        name: "training-2.jpg",
+        alt: "taking to children",
+      },
+      {
+        name: "training-3.jpg",
+        alt: "sharing wisdom",
+      },
+      {
+        name: "training-4.jpg",
+        alt: "talking about health",
+      },
+      {
+        name: "training-5.jpg",
+        alt: "sharing ideas",
+      },
+      {
+        name: "training-6.jpg",
+        alt: "Talking about nutrition",
+      },
+      {
+        name: "training-7.jpg",
+        alt: "Kids havin fun",
+      },
+      {
+        name: "training-8.jpg",
+        alt: "Light lecture",
+      },
+      {
+        name: "training-9.jpg",
+        alt: "kids listening",
+      },
+      {
+        name: "training-10.jpg",
+        alt: "practicising more knowledge",
+      },
+      {
+        name: "training-11.jpg",
+        alt: "kids grasping knowledge about their issues",
+      },
+      {
+        name: "training-12.jpg",
+        alt: "showing charts",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "ASHA Training and Capacity Building",
+    description:
+      "Accredited Social Health Activists (ASHA's) are primarily women selected from their respective villages (one per village/locality) who work as an interface between the community and the public health system. This program aims to develop further skills in ASHA's so they can better represent their communities. Friends contributed to the training in Roorkee.",
+    thumbnail: "1.jpg",
+    folderName: "asha",
+    images: [
+      {
+        name: "1.jpg",
+        alt: "sign",
+      },
+      {
+        name: "2.jpg",
+        alt: "more signage",
+      },
+      {
+        name: "3.jpg",
+        alt: "sharing wisdom",
+      },
+      {
+        name: "4.jpg",
+        alt: "initiating",
+      },
+      {
+        name: "5.jpg",
+        alt: "sharing ideas",
+      },
+      {
+        name: "6.jpg",
+        alt: "Talking about nutrition",
+      },
+      {
+        name: "7.jpg",
+        alt: "ASHAs havin fun",
+      },
+      {
+        name: "8.jpg",
+        alt: "Light lecture",
+      },
+      {
+        name: "9.jpg",
+        alt: "ASHAs listening",
+      },
+      {
+        name: "10.jpg",
+        alt: "practicising more knowledge",
+      },
+      {
+        name: "11.jpg",
+        alt: "ASHAs grasping knowledge about their issues",
+      },
+      {
+        name: "12.jpg",
+        alt: "About health",
+      },
+      {
+        name: "13.jpg",
+        alt: "More Lectures",
+      },
+      {
+        name: "14.jpg",
+        alt: "Intense focus",
+      },
+      {
+        name: "15.jpg",
+        alt: "Learning a lot",
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: "Special Health Camps",
+    description:
+      "We run special health camps quite often as an outreach to the sick and needy who aren't able to make it to our health centres. These are on going camps as part of our Urban Primary health Centres Project. We run 14 of these camps per month in different areas in Roorkee.",
+    thumbnail: "16.jpg",
+    folderName: "special-camps",
+    images: [
+      {
+        name: "0.jpg",
+        alt: "Our Sign",
+      },
+      {
+        name: "16.jpg",
+        alt: "Team member Helping",
+      },
+      {
+        name: "2.jpg",
+        alt: "Kids Study",
+      },
+      {
+        name: "3.jpg",
+        alt: "Giving out food",
+      },
+      {
+        name: "4.jpg",
+        alt: "More food spread",
+      },
+      {
+        name: "5.jpg",
+        alt: "Typical house in slums",
+      },
+      {
+        name: "6.jpg",
+        alt: "Parents with kids",
+      },
+      {
+        name: "7.jpg",
+        alt: "kids huddled up",
+      },
+      {
+        name: "8.jpg",
+        alt: "kids with books",
+      },
+      {
+        name: "10.jpg",
+        alt: "more kids in a group",
+      },
+      {
+        name: "12.jpg",
+        alt: "Kids sitting and drawing",
+      },
+      {
+        name: "14.jpg",
+        alt: "Slum lady volunteer",
+      },
+      {
+        name: "15.jpg",
+        alt: "Parents and their kids",
+      },
+      {
+        name: "1.jpg",
+        alt: "Our staff with kids",
+      },
+      {
+        name: "17.jpg",
+        alt: "mum looked after",
+      },
+    ],
+  },
+];
